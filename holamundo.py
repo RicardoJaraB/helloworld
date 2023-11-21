4a50b4c14b8dffee96ff3561ef4ceba5a9306f36
Ricardo Jara <ricardo.jara@alumnos.ucn.cl>
2023-11-21 11:01:19 -0300

    first commit

---
+++ holamundo.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("UNI_CORR_500_01.txt", skiprows=4, sep="\t", names=['ID','frames','X','Y','Z'])
+st.write("""
+# Mi primera aplicación interactiva
+## Histograma
+         """)
+st.table(df.head())
